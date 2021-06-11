1c0c73160eede4ca0e755ffd81cf9bf40d4807c9
Daphne <daphneboelhouwers@gmail.com>
2021-06-11 14:54:52 +0200

    [Add] Starting Project + Camera

---
+++ Camera.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <glm/glm.hpp>
+
+struct GLFWwindow;
+
+class Camera
+{
+public:
+	Camera(GLFWwindow*);
+
+	glm::mat4 getMatrix();
+	void update(GLFWwindow*);
+
+private:
+	glm::vec3 position = glm::vec3(0, 0, 0);
+	glm::vec2 rotation = glm::vec2(0, 0);
+
+	void move(float angle, float fac);
+};
